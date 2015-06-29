5848042b200db224888673baa5fabd5d83859c93
cvidkal <cgjdemo@gmail.com>
2015-06-29 14:52:26 +0800

    Speed up the ANMS algorithm

--- anms/anms.cpp
+++ anms/anms.cpp
@@ -41,25 +41,28 @@ inline int dist(const KeyPoint &a, const KeyPoint &b)
 
 void anms(std::vector<KeyPoint> & keypoints, const int k, std::vector<int> &idx,cv::Size imgSize)
 {
+	TickMeter tm1, tm2, tm3, tm4;
+	TickMeter tm5;
 	int width = imgSize.width / 20;
 	int height = imgSize.height / 20;
 	int c_robust = 1;
 	static vector<vector<int>> table;
 
+//	tm1.start();
 	if (table.empty())
 	{
 		table.resize(width*height);
-		for (int x = 0; x < width; x++)
+		for (int x = 0; x < width; ++x)
 		{
-			for (int y = 0; y < height; y++)
+			for (int y = 0; y < height; ++y)
 			{
 				int lx = x - 1 < 0 ? 0 : x - 1;
 				int ly = y - 1 < 0 ? 0 : y - 1;
 				int rx = x + 1 >= width ? width - 1 : x + 1;
 				int ry = y + 1 >= height ? height - 1 : y + 1;
-				for (int j = lx; j <= rx; j++)
+				for (int j = lx; j <= rx; ++j)
 				{
-					for (int k = ly; k <= ry; k++)
+					for (int k = ly; k <= ry; ++k)
 					{
 						table[y*width+x].push_back(k*width + j);
 					}
@@ -72,59 +75,76 @@ void anms(std::vector<KeyPoint> & keypoints, const int k, std::vector<int> &idx,
 	mask2.resize(width*height);
 	static vector<vector<int>> bucket;
 	bucket.resize(width*height);
-	for (size_t i = 0; i < bucket.size(); i++)
+	for (size_t i = 0; i < bucket.size(); ++i)
 	{
 		bucket[i].clear();
 	}
 	std::sort(keypoints.begin(), keypoints.end(), [](const KeyPoint &a, const KeyPoint &b){return a.response > b.response; });
+
 	std::vector<std::pair<int, int>> radius;
 	radius.reserve(keypoints.size());
-	for (size_t i = 0; i < keypoints.size(); i++)
+//	tm3.start();
+	for (size_t i = 0; i < keypoints.size(); ++i)
 	{
 		int r = 0xffffff;
 		int x, y;
 		x = keypoints[i].pt.x / 20;
 		y = keypoints[i].pt.y / 20;
-		if (mask[y*width+x])
+		int idx = y*width + x;
+		if (mask[idx])
 		{
-			for (auto j : bucket[y*width+x])
+//			tm2.start();
+			for (int k = 0; k < bucket[idx].size();++k)
 			{
+				int &j = bucket[idx][k];
 				int temp;
-				if (keypoints[i].response <= c_robust*keypoints[j].response)
+//				tm4.start();
+				if (keypoints[i].response < c_robust*keypoints[j].response)
 				{
 					if ((temp = dist(keypoints[i], keypoints[j])) < r)
 						r = temp;
 				}
+//				tm4.stop();
 			}
+//			tm2.stop();
 		}
-		if (!mask2[y*width + x])
+		if (!mask2[idx])
 		{
-			for (auto k : table[y*width + x])
+			for (int j = 0; j < table[idx].size();++j)
 			{
-				mask[k] = 1;
+				mask[table[idx][j]] = 1;
 			}
 		}
-		if (!mask2[y*width + x])
+		if (!mask2[idx])
 		{
-			mask2[y*width + x] = 1;
+			mask2[idx] = 1;
 		}
-		for (auto k : table[y*width + x])
+		for (int j = 0; j < table[idx].size(); ++j)
 		{
-			bucket[k].push_back(i);
+			bucket[table[idx][j]].push_back(i);
 		}
 		radius.push_back(std::pair<int, int>(r, i));
 	}
-
+//	tm3.stop();
 	sort(radius.begin(), radius.end(), [](const std::pair<int, int>&a, const std::pair<int, int>&b){return a.first>b.first; });
 	idx.clear();
+	idx.reserve(500);
 	std::vector<KeyPoint> kptsOut;
+	kptsOut.reserve(500);
 	int size = min((size_t)k, keypoints.size());
-	for (int i = 0; i < size; i++)
+	for (int i = 0; i < size; ++i)
 	{
 		kptsOut.push_back(keypoints[radius[i].second]);
 		idx.push_back(radius[i].second);
 	}
 	keypoints = kptsOut;
+	//tm1.stop();
+	//cout << "a--:" << tm1.getTimeSec() << endl;
+	//cout << "b--:" << tm2.getTimeSec() << endl;
+	//cout << "c--:" << tm3.getTimeSec() << endl;
+	//cout << "d--:" << tm4.getTimeSec() << endl;
+
+
 
 	//TickMeter tm;
 	//idx.clear();
--- anms/main.cpp
+++ anms/main.cpp
@@ -1,16 +1,30 @@
 #include "getMatch.h"
 #include "anms.h"
-
+#include "TickMeter.h"
 
 
 int main()
 {
 	Mat a = imread("1.png");
-	vector<KeyPoint> kpts;
+	Mat b = imread("2.png");
+	vector<KeyPoint> kpts,kpts2;
 	Ptr<FastFeatureDetector> fast = FastFeatureDetector::create();
 	fast->detect(a, kpts);
+	fast->detect(b, kpts2);
+
 	vector<int> idx;
+	TickMeter tm;
+	tm.start();
 	anms(kpts, 500, idx,a.size());
+	tm.stop();
+	cout << tm.getTimeSec() << endl;
+
+	tm.reset();
+	tm.start();
+	anms(kpts2, 500, idx, a.size());
+	tm.stop();
+	cout << tm.getTimeSec() << endl;
+
 	Mat out;
 	drawKeypoints(a, kpts, out);
 	
